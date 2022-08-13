0140419dc6b5c11691d02989ca7edbacd6a02d4f
hahaturbo <aaa980411481@163.com>
2022-08-13 18:48:20 +0800

    feat: 样式调整

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import './app.scss';
-import 'taro-ui/dist/style/index.scss';
 
 // Taro 额外添加的 hooks 要从 '@tarojs/taro' 中引入
 import Taro, { useDidShow, useDidHide, useReady } from '@tarojs/taro';
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import { View, Text } from '@tarojs/components';
-import { AtButton, AtCard, AtForm, AtInput } from 'taro-ui';
+import { AtButton, AtInput } from 'taro-ui';
 import './index.scss';
 import { useState } from 'react';
-import { useDidShow } from '@tarojs/taro';
 import Taro from '@tarojs/taro';
 
 import { create, all } from 'mathjs';
@@ -47,24 +46,28 @@ const calcResult = (dataSource?: SourceType): ResultType | undefined => {
   const result: ResultType = {} as ResultType;
   if (dataSource?.count && dataSource?.unitPrice) {
     result.total = math.format(
-      math.multiply(math.bignumber(Number(dataSource.count)), math.bignumber(Number(dataSource.unitPrice)))
+      math.multiply(math.bignumber(Number(dataSource.count)), math.bignumber(Number(dataSource.unitPrice))),
+      { notation: 'fixed' }
     );
     if (dataSource?.needPrice && dataSource?.lessPrice && result.total) {
       result.totalLess = math.format(
         math.multiply(
           math.floor(Number(math.divide(math.bignumber(result.total), math.bignumber(Number(dataSource.needPrice))))),
           math.bignumber(Number(dataSource.lessPrice))
-        )
+        ),
+        { notation: 'fixed' }
       );
 
       result.realTotal = math.format(
-        math.chain(math.bignumber(result.total)).subtract(math.bignumber(result.totalLess)).done()
+        math.chain(math.bignumber(result.total)).subtract(math.bignumber(result.totalLess)).done(),
+        { notation: 'fixed' }
       );
       result.lessUnit = math.format(
         math
           .chain(math.bignumber(result.realTotal))
           .divide(math.bignumber(Number(dataSource.count)))
-          .done()
+          .done(),
+        { notation: 'fixed' }
       );
     }
   }
@@ -89,71 +92,73 @@ const Index: React.FC<IndexProps> = () => {
 
   return (
     <View className="index">
-      <View className="form">
-        <View>
-          <View className="title">基本信息</View>
+      <View className="content">
+        <View className="form">
+          <View>
+            <View className="title">基本信息</View>
+            <AtInput
+              name="unit_price"
+              title="请输入单价"
+              type="digit"
+              placeholder="输入单价，例如59.8"
+              value={dataSource?.unitPrice}
+              onChange={value => {
+                setDataSource(pre => ({ ...(pre as SourceType), unitPrice: String(value) }));
+              }}
+            />
+            <AtInput
+              name="dataSource?.count"
+              title="请输入数量"
+              type="number"
+              placeholder="输入数量，例如30"
+              value={dataSource?.count}
+              onChange={value => {
+                setDataSource(pre => ({ ...(pre as SourceType), count: String(value) }));
+              }}
+            />
+          </View>
+          <View className="title">折扣信息</View>
           <AtInput
-            name="unit_price"
-            title="请输入单价"
+            name="need_price"
+            title="每满"
             type="digit"
-            placeholder="输入单价，例如59.8"
-            value={dataSource?.unitPrice}
+            placeholder="输入满减金额，例如150"
+            value={dataSource?.needPrice}
             onChange={value => {
-              setDataSource(pre => ({ ...(pre as SourceType), unitPrice: String(value) }));
+              setDataSource(pre => ({ ...(pre as SourceType), needPrice: String(value) }));
             }}
           />
           <AtInput
-            name="dataSource?.count"
-            title="请输入数量"
-            type="number"
-            placeholder="输入数量，例如30"
-            value={dataSource?.count}
+            name="less_price"
+            title="减"
+            type="digit"
+            placeholder="输入折扣金额例如30"
+            value={dataSource?.lessPrice}
             onChange={value => {
-              setDataSource(pre => ({ ...(pre as SourceType), count: String(value) }));
+              setDataSource(pre => ({ ...(pre as SourceType), lessPrice: String(value) }));
             }}
           />
         </View>
-        <View className="title">折扣信息</View>
-        <AtInput
-          name="need_price"
-          title="每满"
-          type="digit"
-          placeholder="输入满减金额，例如150"
-          value={dataSource?.needPrice}
-          onChange={value => {
-            setDataSource(pre => ({ ...(pre as SourceType), needPrice: String(value) }));
-          }}
-        />
-        <AtInput
-          name="less_price"
-          title="减"
-          type="digit"
-          placeholder="输入折扣金额例如30"
-          value={dataSource?.lessPrice}
-          onChange={value => {
-            setDataSource(pre => ({ ...(pre as SourceType), lessPrice: String(value) }));
-          }}
-        />
-      </View>
-      <View className="title">计算结果</View>
-      <View className="result">
-        <View className="item">
-          <Text className="label">原价：</Text>
-          <Text className="content">
-            {dataSource?.unitPrice} × {dataSource?.count} = {result?.total}
-          </Text>
-        </View>
-        <View className="item">
-          <Text className="label">共减去：</Text>
-          <Text className="content">{result?.totalLess}</Text>
-        </View>
-        <View className="item">
-          <Text className="label">最终价格：</Text>
-          <Text className="content">{result?.realTotal}</Text>
-        </View>
-        <View className="item">
-          <Text className="label">折算单价：</Text>
-          <Text className="content">{result?.lessUnit}</Text>
+        <View className="title">计算结果</View>
+        <View className="result">
+          <View className="item">
+            <Text className="label">原价：</Text>
+            <Text className="content">
+              {dataSource?.unitPrice} × {dataSource?.count} = {result?.total}
+            </Text>
+          </View>
+          <View className="item">
+            <Text className="label">共减去：</Text>
+            <Text className="content">{result?.totalLess}</Text>
+          </View>
+          <View className="item">
+            <Text className="label">最终价格：</Text>
+            <Text className="content">{result?.realTotal}</Text>
+          </View>
+          <View className="item">
+            <Text className="label">折算单价：</Text>
+            <Text className="content">{result?.lessUnit}</Text>
+          </View>
         </View>
       </View>
       <View className="btn-container">
